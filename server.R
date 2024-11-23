8a89ae3c2fc60120950de0dd671bdb01410df132
Oliver Brown <156150028+ombrown1@users.noreply.github.com>
2024-11-23 11:19:26 -0800

    Update server.R

--- server.R
+++ server.R
@@ -1,5 +1,4 @@
-function(input, output, session) {
-  
+server <- function(input, output, session) {
   reactive_data <- reactive({
     list(
       gdp = gdp_data %>% filter(Year >= input$year_range[1], Year <= input$year_range[2]),
@@ -8,79 +7,69 @@ function(input, output, session) {
     )
   })
   
-  output$maps <- renderPlot({
+  output$bar_plots <- renderPlot({
     data_list <- reactive_data()
-    selected_years <- c(input$year_range[1], input$year_range[2])
     variables <- input$variables
-    plots <- list()
+    year_range <- c(input$year_range[1], input$year_range[2])
+    change_type <- input$change_type
     
-    for (variable in variables) {
-      for (year in selected_years) {
-        if (variable == "gdp") {
-          plot <- process_and_plot_map(data_list$gdp, "GDP", "GDP", year, world)
-        } else if (variable == "population") {
-          plot <- process_and_plot_map(data_list$population, "Population", "Population", year, world)
-        } else if (variable == "co2") {
-          plot <- process_and_plot_map(data_list$co2, "CO2_Emissions", "CO2 Emissions", year, world)
-        }
-        plots <- c(plots, list(plot))
+    if (change_type == "percentage") {
+      all_percentage_changes <- unlist(lapply(variables, function(variable) {
+        data <- data_list[[variable]]
+        value_col <- if (variable == "gdp") "GDP" else if (variable == "population") "Population" else "CO2_Emissions"
+        start_year <- as.integer(year_range[1])
+        end_year <- as.integer(year_range[2])
+        
+        data_filtered <- data %>%
+          filter(Year %in% c(start_year, end_year)) %>%
+          dplyr::select(Year, Country, !!sym(value_col)) %>%
+          pivot_wider(names_from = Year, values_from = !!sym(value_col)) %>%
+          drop_na(c(as.character(start_year), as.character(end_year))) %>%
+          filter(!!sym(as.character(start_year)) != 0) %>%
+          mutate(
+            Percentage_Change = ((!!sym(as.character(end_year))) - (!!sym(as.character(start_year)))) / abs(!!sym(as.character(start_year))) * 100
+          ) %>%
+          filter(is.finite(Percentage_Change)) %>%
+          pull(Percentage_Change)
+      }))
+      
+      max_limit <- ceiling(max(all_percentage_changes, na.rm = TRUE))
+      min_limit <- floor(min(all_percentage_changes, na.rm = TRUE))
+      global_limits <- list(percentage = c(min_limit, max_limit))
+    } else {
+      global_limits <- list(absolute = list())
+      for (variable in variables) {
+        data <- data_list[[variable]]
+        value_col <- if (variable == "gdp") "GDP" else if (variable == "population") "Population" else "CO2_Emissions"
+        start_year <- as.integer(year_range[1])
+        end_year <- as.integer(year_range[2])
+        
+        changes <- data %>%
+          filter(Year %in% c(start_year, end_year)) %>%
+          dplyr::select(Year, Country, !!sym(value_col)) %>%
+          pivot_wider(names_from = Year, values_from = !!sym(value_col)) %>%
+          drop_na(c(as.character(start_year), as.character(end_year))) %>%
+          mutate(
+            Change = (!!sym(as.character(end_year))) - (!!sym(as.character(start_year)))
+          ) %>%
+          pull(Change)
+        
+        max_limit <- ceiling(max(changes, na.rm = TRUE))
+        min_limit <- floor(min(changes, na.rm = TRUE))
+        global_limits$absolute[[variable]] <- c(min_limit, max_limit)
       }
     }
     
-    wrap_plots(plots, ncol = 2)
+    plot <- process_and_plot_bar(data_list, variables, year_range, global_limits, change_type)
+    plot
   })
   
   output$line_plots <- renderPlot({
     data_list <- reactive_data()
     variables <- input$variables
-    year_range <- input$year_range
+    year_range <- c(input$year_range[1], input$year_range[2])
     
-    map_countries <- world$iso_a3 %>% na.omit() %>% unique()
-    
-    all_countries <- unique(unlist(lapply(variables, function(variable) {
-      if (variable == "gdp") {
-        data_list$gdp %>% filter(Country_Code %in% map_countries) %>% pull(Country)
-      } else if (variable == "population") {
-        data_list$population %>% filter(Country_Code %in% map_countries) %>% pull(Country)
-      } else if (variable == "co2") {
-        data_list$co2 %>% filter(Country_Code %in% map_countries) %>% pull(Country)
-      }
-    })))
-    
-    plots <- list()
-    
-    for (variable in variables) {
-      if (variable == "gdp") {
-        filtered_data <- data_list$gdp %>% filter(Country_Code %in% map_countries)
-        top_countries <- filtered_data %>%
-          group_by(Country) %>%
-          summarize(MaxValue = max(GDP, na.rm = TRUE)) %>%
-          arrange(desc(MaxValue)) %>%
-          slice_head(n = 5) %>%
-          pull(Country)
-        plot <- process_and_plot_line(filtered_data, "GDP", "GDP", top_countries, year_range, map_countries)
-      } else if (variable == "population") {
-        filtered_data <- data_list$population %>% filter(Country_Code %in% map_countries)
-        top_countries <- filtered_data %>%
-          group_by(Country) %>%
-          summarize(MaxValue = max(Population, na.rm = TRUE)) %>%
-          arrange(desc(MaxValue)) %>%
-          slice_head(n = 5) %>%
-          pull(Country)
-        plot <- process_and_plot_line(filtered_data, "Population", "Population", top_countries, year_range, map_countries)
-      } else if (variable == "co2") {
-        filtered_data <- data_list$co2 %>% filter(Country_Code %in% map_countries)
-        top_countries <- filtered_data %>%
-          group_by(Country) %>%
-          summarize(MaxValue = max(CO2_Emissions, na.rm = TRUE)) %>%
-          arrange(desc(MaxValue)) %>%
-          slice_head(n = 5) %>%
-          pull(Country)
-        plot <- process_and_plot_line(filtered_data, "CO2_Emissions", "CO2 Emissions", top_countries, year_range, map_countries)
-      }
-      plots <- c(plots, list(plot))
-    }
-    
-    wrap_plots(plots, ncol = 3)
+    plot <- process_and_plot_line(data_list, variables, year_range)
+    plot
   })
 }
