cef0c96c9bad089c971ec42d734c87ecf395d934
Thy Luong <147956296+teaiced@users.noreply.github.com>
2024-11-23 11:16:21 -0800

    Add files via upload

--- shiny_app/pop_shiny_app.R
+++ shiny_app/pop_shiny_app.R
@@ -3,11 +3,65 @@ library(tidyverse)
 library(ggplot2)
 library(forcats)
 
+gdp_data <- read_csv("data/gdp.csv", skip = 4, show_col_types = FALSE) %>%
+  select(-contains("...")) %>%
+  rename(Country = `Country Name`, Country_Code = `Country Code`) %>%
+  pivot_longer(cols = matches("^\\d{4}$"), names_to = "Year", values_to = "GDP") %>%
+  mutate(Year = as.integer(Year),
+         Country = recode(Country, "Russian Federation" = "Russia"),
+         Country = recode(Country, "World" = "Global"))
+
+pop_data <- read_csv("data/pop.csv", skip = 4, show_col_types = FALSE) %>%
+  select(-contains("...")) %>%
+  rename(Country = `Country Name`, Country_Code = `Country Code`) %>%
+  pivot_longer(cols = matches("^\\d{4}$"), names_to = "Year", values_to = "Population") %>%
+  mutate(Year = as.integer(Year),
+         Country = recode(Country, "Russian Federation" = "Russia"),
+         Country = recode(Country, "World" = "Global"),
+         Population = Population/1e9)
+
+co2_data <- read_csv("data/co2.csv", show_col_types = FALSE) %>%
+  select(-contains("...")) %>%
+  rename(
+    Country = `Country`,
+    CO2_Emissions = `Total`,
+    Country_Code = `ISO 3166-1 alpha-3`
+  ) %>%
+  mutate(
+    Year = as.integer(Year),
+    Country = recode(Country, "USA" = "United States")
+  )
+
+setdiff(pop_data$Country, gdp_data$Country)
+setdiff(pop_data$Country, co2_data$Country)
+setdiff(pop_data$Country, co2_data$Country)
+
+common_countries <- Reduce(intersect, list(gdp_data$Country, pop_data$Country, co2_data$Country))
+
+gdp_data <- gdp_data %>% filter(Country %in% common_countries)
+pop_data <- pop_data %>% filter(Country %in% common_countries)
+co2_data <- co2_data %>% filter(Country %in% common_countries)
+
+setdiff(pop_data$Country, gdp_data$Country)
+setdiff(pop_data$Country, co2_data$Country)
+setdiff(pop_data$Country, co2_data$Country)
+
+
+
+pop <- read.csv("data/pop.csv", skip = 4)
+pop <- pop %>%
+  pivot_longer(
+    cols = starts_with("X"), 
+    names_to = "year",          
+    values_to = "pop"
+  )
+
+
 ui <- fluidPage(
   
     sliderInput("years", "Choose the year range", min=1961, max=2023, value=c(1961, 2023), sep=""),
     selectInput("countries", "Select countries:", 
-                choices = unique(pop$Country.Name),
+                choices = unique(common_countries),
                 multiple = TRUE),
     
     plotOutput("pop_line")
@@ -16,29 +70,14 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-    pop <- read.csv("data/pop.csv", skip = 4)
-  pop <- pop %>%
-    pivot_longer(
-      cols = starts_with("X"), 
-      names_to = "year",          
-      values_to = "pop"
-    )
-  pop$year <- substr(pop$year, 2, nchar(pop$year))
-  pop$year <- as.numeric(pop$year)
-  pop <- pop %>% filter(Indicator.Name == "Population, total")
-  pop$Country.Name <- replace(pop$Country.Name, pop$Country.Name == "Russian Federation", "Russia")
-  pop$Country.Name <- replace(pop$Country.Name, pop$Country.Name == "World", "Global")
-  pop$pop <- pop$pop/1e9
-  
-  
   filtered_pop <- reactive({
-     pop %>% filter(year >= input$years[1] & year <= input$years[2])
+     pop_data %>% filter(Year >= input$years[1] & Year <= input$years[2])
   })
   
   output$pop_line <- renderPlot({
-    ggplot(filtered_pop() %>% filter(Country.Name %in% input$countries), 
-           aes(x=year, y=pop, color=Country.Name)) +
-      geom_line(aes(color=Country.Name)) +
+    ggplot(filtered_pop() %>% filter(Country %in% input$countries), 
+           aes(x=Year, y=Population, color=Country)) +
+      geom_line(aes(color=Country)) +
       scale_color_brewer(palette = "Set1") +
       
       ggtitle("How has population changed over time?") +
