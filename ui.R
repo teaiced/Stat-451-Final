1b7e765fb460e127a37c76fab0af8167a3677b2a
Oliver Brown <156150028+ombrown1@users.noreply.github.com>
2024-11-24 14:20:21 -0800

    Update ui.R
    Added conditional stuff, so the some options will only show up on panels relevant to those options

--- ui.R
+++ ui.R
@@ -8,30 +8,62 @@ ui <- fluidPage(
   titlePanel("Global Trends in GDP, Population, and CO2 Emissions"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("year_range", "Select Year Range:", min = start_year, max = end_year, value = c(1990, 2010), step = 1, sep = ""),
-      checkboxGroupInput("variables", "Select Variables to Display:",
-                         choices = c("GDP" = "gdp", "Population" = "population", "CO2 Emissions" = "co2"),
-                         selected = c("gdp", "population", "co2")),
-      selectizeInput("countries", "Select up to 5 countries:", 
-                     choices = unique(common_countries),
-                     selected = "Global",
-                     multiple = TRUE,
-                     options = list(maxItems = 5)),
-      radioButtons("change_type", "Select Change Type:",
-                   choices = c("Percentage Change" = "percentage"),
-                   selected = "percentage"),
-      selectInput("scatter_x", "Select X-axis Variable for Scatter Plot:",
-                  choices = c("GDP" = "gdp", "Population" = "population", "CO2 Emissions" = "co2")),
-      selectInput("scatter_y", "Select Y-axis Variable for Scatter Plot:",
-                  choices = c("GDP" = "gdp", "Population" = "population", "CO2 Emissions" = "co2")),
+      sliderInput(
+        "year_range",
+        "Select Year Range:",
+        min = start_year,
+        max = end_year,
+        value = c(1990, 2010),
+        step = 1,
+        sep = ""
+      ),
+      checkboxGroupInput(
+        "variables",
+        "Select Variables to Display:",
+        choices = c("GDP" = "gdp", "Population" = "population", "CO2 Emissions" = "co2"),
+        selected = c("gdp", "population", "co2")
+      ),
+      selectizeInput(
+        "countries",
+        "Select up to 5 countries:",
+        choices = unique(common_countries),
+        selected = "Global",
+        multiple = TRUE,
+        options = list(maxItems = 5)
+      ),
+      # Conditional "Change Type" input
+      conditionalPanel(
+        condition = "input['tabs'] == 'Bar Plot'",
+        radioButtons(
+          "change_type",
+          "Select Change Type:",
+          choices = c("Percentage Change" = "percentage"),
+          selected = "percentage"
+        )
+      ),
+      # Conditional scatter plot inputs
+      conditionalPanel(
+        condition = "input['tabs'] == 'Scatter Plot'",
+        selectInput(
+          "scatter_x",
+          "Select X-axis Variable for Scatter Plot:",
+          choices = c("GDP" = "gdp", "Population" = "population", "CO2 Emissions" = "co2")
+        ),
+        selectInput(
+          "scatter_y",
+          "Select Y-axis Variable for Scatter Plot:",
+          choices = c("GDP" = "gdp", "Population" = "population", "CO2 Emissions" = "co2"),
+          selected = "population"
+        )
+      ),
       helpText("Use the slider to select year range and checkboxes to choose variables.")
     ),
-    
     mainPanel(
       tabsetPanel(
-        tabPanel("Bar Plot", plotOutput("bar_plots", height = "800px")),
-        tabPanel("Line Plot", plotOutput("line_plots", height = "400px")),
-        tabPanel("Scatter Plot", plotOutput("scatter_plot", height = "400px"))
+        id = "tabs",  # Add an ID to the tabsetPanel for referencing
+        tabPanel("Bar Plot", plotOutput("bar_plots", height = "1200px")),
+        tabPanel("Line Plot", plotOutput("line_plots", height = "1200px")),
+        tabPanel("Scatter Plot", plotOutput("scatter_plot", height = "1200px"))
       )
     )
   )
